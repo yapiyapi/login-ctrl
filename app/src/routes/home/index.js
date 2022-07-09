27ae2431d78b18733b132aec97bb65a9dda8ae19
unknown <kyh10927roll@gmail.com>
2022-07-09 16:55:20 +0900

    회원가입 라우팅

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,7 +6,9 @@ const ctrl = require('./home.ctrl');
 router.get('/',ctrl.output.home);     //get => 출력
 router.get('/login',ctrl.output.login);
 router.get('/register',ctrl.output.register);
+
 router.post('/login',ctrl.process.login);     //post => 처리
+router.post('/register',ctrl.process.register);     //post => 처리
 
 
 module.exports = router;

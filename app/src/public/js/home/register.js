0ed9a5856e082919505c683017609fd4682a8eaf
unknown <kyh10927roll@gmail.com>
2022-07-09 15:22:34 +0900

    회원가입 요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const id = document.querySelector('#id'),
+name = document.querySelector('#name'),
+
+psword = document.querySelector('#psword'),
+confirmPsword = document.querySelector('#confirm-psword'),
+loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', register);
+
+
+
+function register(){
+    const req = {
+        id : id.value,
+        name: name.value,
+        psword : psword.value,
+        confirmPsword : confirmPsword.value,
+
+    };
+    console.log(req)
+
+    fetch('/register',{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(req),
+    }).then((res)  => res.json())
+    .then((res)=> {
+        if(res.success){
+            location.href="/login";
+        }else{
+            alert(res.msg);
+        };
+    }).catch((err)=>{
+        console.error(new Error('로그인 중 에러 발생'));
+    })
+}
